5b37788a253c8790e9eccf652264a33ae9c06ddb
Tierney Cyren <hello@bnb.im>
2022-02-07 21:52:04 -0500

    fix: jason was right and some other stuff
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 async function action() {
-  const importOrg = core.getInput('organization', { required: true })
-  const privlidgedToken = core.getInput('token', { required: true })
-  const octokit = new github.getOctokit(privlidgedToken)
+  const org = core.getInput('organization', { required: true })
+  const privilegedToken = core.getInput('token', { required: true })
+  const octokit = new github.getOctokit(privilegedToken)
 
-  const blockedUsers = await octokit.rest.orgs.listBlockedUsers({ importOrg })
+  const blockedUsers = await octokit.rest.orgs.listBlockedUsers({ org })
   console.log(`Blocked users: ${blockedUsers}`)
 }
 
