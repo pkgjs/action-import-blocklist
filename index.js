c2a296c42a561fdc48a82597a7ada7bdef2c7524
Tierney Cyren <hello@bnb.im>
2022-02-07 20:17:30 -0500

    chore: some code cleanup
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-const importOrg = core.getInput('organization', { required: true })
-
 async function action() {
+  const importOrg = core.getInput('organization', { required: true })
   const privlidgedToken = core.getInput('token', { required: true })
-
   const octokit = new github.getOctokit(privlidgedToken)
 
   const blockedUsers = await octokit.rest.orgs.listBlockedUsers({ importOrg })
